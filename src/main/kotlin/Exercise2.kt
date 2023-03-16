bbc7915a027726c2200f0ce46f41c5511a8906e9
ntunyuserge <ntunyuserge@gmail.com>
2023-03-16 13:46:01 +0100

    Exercise2 complete

---
+++ src/main/kotlin/Exercise2.kt
@@ -0,0 +1,12 @@
+// Print the odd numbers less than 100
+fun main() {
+    var i: Int = 1
+    while(i<101){
+        if(i % 2 != 0){
+            println(i);
+            i++
+        }
+        i++
+    }
+
+}
---
+++ src/main/kotlin/practice.kt
@@ -0,0 +1,8 @@
+var name: String = "Serge"
+var greeting: String? = null
+fun main(){
+    if (greeting != null){
+        println(greeting)
+    }
+    println(name)
+}
