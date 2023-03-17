b082574bb137f981345a83f55668509d7513856c
ntunyuserge <ntunyuserge@gmail.com>
2023-03-17 20:32:43 +0100

    Exercise5 complete

---
+++ src/main/kotlin/Exercise5.kt
@@ -0,0 +1,11 @@
+// Calculate the sum of numbers from 1 to 10
+
+fun main() {
+    var i: Int = 0
+    var ans: Int = 0
+    while (i < 11){
+        ans += i
+        i++
+    }
+    println(" Sum of first 10 numbers is: $ans")
+}
