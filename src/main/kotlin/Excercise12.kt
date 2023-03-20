9d8d07f2ceda983e5a394370829ee65b1ab0d26c
ntunyuserge <ntunyuserge@gmail.com>
2023-03-20 16:18:08 +0100

    Started Exercise12

---
+++ src/main/kotlin/Excercise12.kt
@@ -0,0 +1,7 @@
+// Create a function that receives an array of numbers as argument and returns an
+//array containing only the positive numbers
+
+fun main() {
+    var myArray: Array <Int> = arrayOf(2,3,5)
+    println(myArray)
+}
