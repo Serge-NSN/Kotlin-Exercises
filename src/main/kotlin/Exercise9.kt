bf8b66a2471288c2d410efe71245788f03a5beee
ntunyuserge <ntunyuserge@gmail.com>
2023-03-20 15:51:02 +0100

    Exercise9 complete

---
+++ src/main/kotlin/Exercise9.kt
@@ -0,0 +1,7 @@
+// Create a function that will convert from Fahrenheit to Celsius
+
+fun fahrenheitToCelcius(fahrenheit: Float) = (fahrenheit - 32) * 5/9
+
+fun main() {
+    println(fahrenheitToCelcius(98.6F))
+}
