9ffcea9316fd62c695da5ec02701857fa74b0747
ntunyuserge <ntunyuserge@gmail.com>
2023-03-21 11:53:05 +0100

    Completed Exercise12

--- src/main/kotlin/Excercise12.kt
+++ src/main/kotlin/Excercise12.kt
@@ -1,7 +1,17 @@
 // Create a function that receives an array of numbers as argument and returns an
 //array containing only the positive numbers
+fun arrOfPositiveNumbers(array: Array <Int>): MutableList<Int>{
+    var list = array.toMutableList()
+    var ans: MutableList<Int> = mutableListOf()
+    for (elt in array){
+        if (elt > 0){
+            ans.add(elt)
+        }
+    }
+    return ans
+}
 
 fun main() {
-    var myArray: Array <Int> = arrayOf(2,3,5)
-    println(myArray)
+    var myArray: Array <Int> = arrayOf(2,3,5,-6,12,-3,0,7)
+    println(arrOfPositiveNumbers(myArray))
 }
