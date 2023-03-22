3d6017da42f88a34c48b386468e3c36e7e7861b5
ntunyuserge <ntunyuserge@gmail.com>
2023-03-22 15:44:37 +0100

    Adjusted isPrime function

--- src/main/kotlin/Excercise16.kt
+++ src/main/kotlin/Excercise16.kt
@@ -3,30 +3,29 @@ import kotlin.math.sqrt
 // Create a function that will return a Boolean specifying if a number is prime
 
 fun isPrime(n: Int): Boolean{
-    var ans: Boolean = false
+    var ans: Boolean = true
     if(n == 0 || n==1){
         ans = false
     }
     else if(n==2){
         ans = true
     }
+    else if(n%2==0){
+        ans = false
+    }
     else{
         var i: Int = 3
-        while(i<= sqrt(n.toDouble())){
+        while(i<= sqrt(n.toFloat())){
             if(n%i==0){
-                i+=2
                 ans = false
                 break
             }
-            else{
-                i+=2
-                ans = true
-            }
+            i+=2
         }
     }
     return ans
 }
 
 fun main() {
-    println(isPrime(131))
+    println(isPrime(10))
 }
