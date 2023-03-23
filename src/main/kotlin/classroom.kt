c0036bf22268f80c1ed0172dfd282d0f5cfba978
ntunyuserge <ntunyuserge@gmail.com>
2023-03-23 11:40:20 +0100

    added simulation of benches function

--- src/main/kotlin/classroom.kt
+++ src/main/kotlin/classroom.kt
@@ -2,14 +2,34 @@ import kotlin.math.roundToInt
 import kotlin.math.roundToLong
 
 class Classroom(){
-//    var students: Int = 20
+    var students: List<String> = listOf("John", "Peter", "Mary", "Suh", "Ngwa", "Che", "Siri", "Njong", "Feh", "Helen", "Paul", "Obi", "Lynn", "Ambe", "Nelson", "Joshua", "Emma", "Serge", "Anna", "Faith")
 
-    fun benchedNeeded(students: Int =20):Int{
-        return (students.toFloat()/3).roundToInt()
+    fun benchesNeeded():Int{
+        return (students.size.toFloat()/3).roundToInt()
     }
+
+    fun displayPositions(){
+        var benchNum = 1
+        var j = 0
+        var k = 0
+        while(benchNum<benchesNeeded()+1){
+            print("Bench $benchNum: ")
+            while(j<3){
+                print(students[k] + " ")
+                j++
+                k++
+            }
+            j=0
+            println("")
+            benchNum++
+        }
+
+        }
 }
 
 fun main() {
     var f1 = Classroom()
-    println(f1.benchedNeeded())
+//    println(f1.benchesNeeded())
+    println(f1.displayPositions())
+
 }
