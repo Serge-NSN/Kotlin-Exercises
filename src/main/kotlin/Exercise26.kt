b0b1e203db57bef9b54870d1a8edcf6db99d951a
ntunyuserge <ntunyuserge@gmail.com>
2023-03-25 20:50:59 +0100

    Exercise26 complete - returning array of elements found only in first array

--- src/main/kotlin/Exercise26.kt
+++ src/main/kotlin/Exercise26.kt
@@ -1,6 +1,16 @@
 // Create a function that will receive two arrays and will return an array with
 //elements that are in the first array but not in the second
 
-fun eltInOnlyFirstArray(arr1: Array<Int>, arr2: Array<Int>): List<Int>{
+fun eltInOnlyFirstArray(arr1: Array<Int>, arr2: Array<Int>): MutableList<Int>{
+    var found = mutableListOf<Int>()
+    for (elt in arr1){
+        if (elt !in arr2){
+            found.add(elt)
+        }
+    }
+    return found
+}
 
+fun main() {
+    println(eltInOnlyFirstArray(arrayOf(1,5,6,12), arrayOf(1,2,3,4,6)))
 }
