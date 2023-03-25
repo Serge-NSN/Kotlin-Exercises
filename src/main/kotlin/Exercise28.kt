49e6918316a7f83c30fef52b3f464980903ce09d
ntunyuserge <ntunyuserge@gmail.com>
2023-03-25 21:14:54 +0100

    Exercise28 complete - Returning sum of first 100 prime numbers

---
+++ src/main/kotlin/Exercise28.kt
@@ -0,0 +1,16 @@
+// Calculate the sum of first 100 prime numbers and return them in an array
+fun sumOfFirstHundredPrimeNumbers():Int{
+    var ans = mutableListOf<Int>()
+    var i = 0
+    while (ans.size < 100){
+        if(isPrime(i)){
+            ans.add(i)
+        }
+        i++
+    }
+    return ans.sum()
+}
+
+fun main() {
+    println(sumOfFirstHundredPrimeNumbers())
+}
