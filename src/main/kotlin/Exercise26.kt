2b8e1fc97803dedcd9f3c486cc9e65c83dd5b18b
ntunyuserge <ntunyuserge@gmail.com>
2023-03-25 20:40:24 +0100

    Started Exercise26

---
+++ src/main/kotlin/Exercise26.kt
@@ -0,0 +1,6 @@
+// Create a function that will receive two arrays and will return an array with
+//elements that are in the first array but not in the second
+
+fun eltInOnlyFirstArray(arr1: Array<Int>, arr2: Array<Int>): List<Int>{
+
+}
