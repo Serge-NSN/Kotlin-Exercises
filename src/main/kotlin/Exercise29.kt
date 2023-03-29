701599e4a73febaa89ac82e88053a94bbcceb620
ntunyuserge <ntunyuserge@gmail.com>
2023-03-29 15:51:45 +0100

    Exercise29 complete

--- src/main/kotlin/Exercise29.kt
+++ src/main/kotlin/Exercise29.kt
@@ -1,1 +1,13 @@
 // Print the distance between the first 100 prime numbers
+fun main() {
+    var last_prime = 2
+    var i = last_prime + 1
+    //var found_prime = 1
+        while (i<100){
+            if(isPrime(i)){
+                println("${i - last_prime} -------- ($i - $last_prime)")
+                last_prime = i
+            }
+            i++
+    }
+}
